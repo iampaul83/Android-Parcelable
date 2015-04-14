ca2d9827503894d12a5b3c4e1912064483cdbd23
iampaul83 <iampaul783@gmail.com>
2015-04-15 00:19:55 +0800

    Put `ParcelableArrayListExtra` to intent, get `ParcelableArrayListExtra` from intent

--- app/src/main/java/com/iampaul83/parcelable/MainActivity.java
+++ app/src/main/java/com/iampaul83/parcelable/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -19,8 +21,24 @@ public class MainActivity extends ActionBarActivity {
      */
     public void onGoAction(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        // TODO: put custom class to intent
+        intent.putParcelableArrayListExtra("data", getFakeDatas());
         startActivity(intent);
+    }
+
+    private ArrayList<MyData> getFakeDatas() {
+        MyData fakeData1 = new MyData("Paul", "Tsai", 25,
+            new SubData("phone", "0912345678"),
+            new SubData("email", "iampaul783@gmail.com")
+        );
+        MyData fakeData2 = new MyData("John", "Chen", 35,
+            new SubData("phone", "0987654321"),
+            new SubData("email", "johnchen123@yahoo.com.tw"),
+            new SubData("address", "桃園市中壢區太麻里隔壁")
+        );
+        ArrayList<MyData> list = new ArrayList<>();
+        list.add(fakeData1);
+        list.add(fakeData2);
+        return list;
     }
 
 }
--- app/src/main/java/com/iampaul83/parcelable/SecondActivity.java
+++ app/src/main/java/com/iampaul83/parcelable/SecondActivity.java
@@ -2,6 +2,9 @@ package com.iampaul83.parcelable;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 
 public class SecondActivity extends ActionBarActivity {
@@ -10,8 +13,8 @@ public class SecondActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        // TODO: retrieve passed data from intent
+        ArrayList<MyData> datas = getIntent().getParcelableArrayListExtra("data");
+        Log.i("datas", "datas => " + datas.toString());
     }
 
 }
